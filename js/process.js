a97507c8fd243a114ade388420c82aeeed089804
Miguel's Crazy Sandbox <87053584+MiguelKingofcodes@users.noreply.github.com>
2024-07-15 08:51:28 -0300

    Add files via upload

--- js/process.js
+++ js/process.js
@@ -44,18 +44,22 @@ function download(data, filename, type) {
 }
 
 function save_ficha(){
-    
+
     // Obtendo características do personagem...
     let p_name = document.getElementById('p_name').value;
     let p_years = document.getElementById('p_years').value;
     let p_ocupation = document.getElementById('p_ocupation').value;
     let p_fightstyle = document.getElementById('p_fightstyle').value;
+    
+    
     let root_image = document.getElementById("photo-input").value;
-    let p_image = document.getElementById("p_image").getAttribute('src');
+    
+    let p_image;
+
+    p_image = document.getElementById("p_image").getAttribute('src');
+    
 
     // Obtendo as cores das fichas...
-
-
 
     // Obtendo os atributos do personagem...
     let perAttr = [document.getElementById("for-input").value, 
@@ -63,47 +67,71 @@ function save_ficha(){
                 document.getElementById("int-input").value,
                 document.getElementById("car-input").value];
 
+    // Barras de atributos
+    let bar = '';
+    let attrBars = [];
+    for (let i=0; i<=perAttr.length; i++){
+        for (let num=0; num<=perAttr[i]; num++){
+            bar += '/'
+            
+        }
+        bar += " (" + perAttr[i] + ")"
+        attrBars.push(bar);
+        bar = ''
+    }
 
 
     // Gera o html contendo os dados da ficha...
-    let ficha_value = '<!DOCTYPE html>' +
+    let ficha_value = '<!DOCTYPE html>'+
+    '<html lang="pt-br">'+
+    
     '<head>'+
+        '<meta charset="UTF-8">'+
+        '<meta name="viewport" content="width=device-width, initial-scale=1.0">' +
     '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">'+
     '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>'+
     '<style>'+
     'body{'+
     'background-color:' + document.body.style.backgroundColor + ';'+
     'color: ' + document.body.style.color + ';' +
-    'font-size: 3em;'+
     '}'+
     '#attribute_box{'+
     'background-color:' + document.getElementById("attribute-box").style.backgroundColor + ';'+
     'color:' + document.getElementById("attribute-box").style.color + ';'+
+    '}'+
+    '.attrBlock{'+
+    'width: 10px;'+
+    'height: 10px;'+
+    'border-radius: 200px;'+
+    'background-color: Black;'+
     '}'+
     '</style>' +
     '</head>'+
     '<body>'+
         '<div class="container-fluid p-2">'+
             '<img class="mb-3" src="' + p_image + '" width="100%">' +
-            '<textarea style="background-color: '+ document.body.style.backgroundColor + '; color: ' + document.body.style.color + '; opacity: 3;" class="container-fluid">Nome: '+ p_name + '</textarea>' +
-            '<textarea style="background-color: '+ document.body.style.backgroundColor + '; color: ' + document.body.style.color + '; opacity: 3;" class="container-fluid">Idade: '+ p_years + '</textarea>' +
-            '<textarea style="background-color: '+ document.body.style.backgroundColor + '; color: ' + document.body.style.color + '; opacity: 3;" class="container-fluid">Ocupação: '+ p_ocupation + '</textarea>' +
-            '<textarea style="background-color: '+ document.body.style.backgroundColor + '; color: ' + document.body.style.color + '; opacity: 3;" class="container-fluid">Estilo de luta: '+ p_fightstyle + '</textarea>' +
+            '<textarea style="font-size: 1.3em; background-color: '+ document.body.style.backgroundColor + '; color: ' + document.body.style.color + '; opacity: 3;" class="container-fluid">Nome: '+ p_name + '</textarea>' +
+            '<textarea style="font-size: 1.3em; background-color: '+ document.body.style.backgroundColor + '; color: ' + document.body.style.color + '; opacity: 3;" class="container-fluid">Idade: '+ p_years + '</textarea>' +
+            '<textarea style="font-size: 1.3em; background-color: '+ document.body.style.backgroundColor + '; color: ' + document.body.style.color + '; opacity: 3;" class="container-fluid">Ocupação: '+ p_ocupation + '</textarea>' +
+            '<textarea style="font-size: 1.3em; background-color: '+ document.body.style.backgroundColor + '; color: ' + document.body.style.color + '; opacity: 3;" class="container-fluid">Estilo de luta: '+ p_fightstyle + '</textarea>' +
         '</div>'+
-        '<div style="font-size:1em;" class="container-fluid" id="attribute_box">'+
+        '<div class="container-fluid" id="attribute_box">'+
             '<h1> Atributos:  </h1>'+
-            '<p>HP: ' + document.getElementById("hp-value").innerText + '</p>' + 
-            '<p>San: ' + document.getElementById("san-value").innerText + '</p>'+
-            '<p> Força: '+ perAttr[0] +'</p>' +
-            '<p> Inteligência: '+ perAttr[1] +'</p>' +
-            '<p> Vigor: '+ perAttr[2] +'</p>' +
-            '<p> Carisma: '+ perAttr[3] +'</p>' +
+            '<h2>HP: ' + document.getElementById("hp-value").innerText + '</h2>' + 
+            '<h2>San: ' + document.getElementById("san-value").innerText + '</h2>'+
+            '<h4> Força: </h2><div class="d-flex flex-row">'+ attrBars[0] +'</div>' +
+            '<h4> Vigor: </h2><div class="d-flex flex-row">'+ attrBars[1] +'</div>' +
+            '<h4> Inteligência: </h2><div class="d-flex flex-row">'+ attrBars[2] +'</div>' +
+            '<h4> Carisma: </h2><div class="d-flex flex-row">'+ attrBars[3] +'</div>' +
         '</div>'+
-        '<h1>Inventário: </h1>'+
-        '<textarea class="form-control" style="background-color: '+ document.body.style.backgroundColor + '; color: ' + document.body.style.color + '; opacity: 3; font-size: 1.3em;">'+ document.getElementById("bag_input").value +'</textarea>' +
-        '<h1>História</h1>'+
-        '<textarea class="form-control" style="background-color: '+ document.body.style.backgroundColor + '; color: ' + document.body.style.color + '; opacity: 3; font-size: 1.3em;">'+ document.getElementById("history_input").value +'</textarea>' +    
-    '</body>'
+        '<div class="container-fluid p-2">'+
+            '<h1>Inventário: </h1>'+
+            '<textarea rows="' + document.getElementById("bag_input").value + '" class="padding: 10px; form-control" style="background-color: '+ document.body.style.backgroundColor + '; color: ' + document.body.style.color + '; opacity: 3; font-size: 1.3em;">'+ document.getElementById("bag_input").value +'</textarea>' +
+            '<h1>História</h1>'+
+            '<textarea class="padding: 10px; form-control" style="background-color: '+ document.body.style.backgroundColor + '; color: ' + document.body.style.color + '; opacity: 3; font-size: 1.3em;">'+ document.getElementById("history_input").value +'</textarea>' +    
+        '</div>'+    
+        '</body>'+
+    '</html>'
 
     // Salva o html
     //download(ficha_value, p_name, 'text/html');
@@ -184,22 +212,23 @@ function changeStats(){
     let hp = 12 + parseInt(document.getElementById("vig-input").value);
     let san = 10 + parseInt(document.getElementById("int-input").value);
 
-    document.getElementById("hp-value").innerText = "";
+    document.getElementById("hp-value").innerHTML = "";
+    
     let hp_value = "";
     for (let i=1; i <= hp; i++){
         hp_value += "/";
         
     }
-    document.getElementById("hp-value").innerText += hp_value + " (" + hp + ")";
+    document.getElementById("hp-value").innerHTML += hp_value + " (" + hp + "/12)";
 
 
+    document.getElementById("san-value").innerHTML = "";
 
-    document.getElementById("san-value").innerText = "";
     let san_value = "";
     for (let i=1; i <= san; i++){
         san_value += "/";
     }
-    document.getElementById("san-value").innerText += san_value + " (" + san + ")";
+    document.getElementById("san-value").innerHTML += san_value + " (" + san + "/10)";
 }
 
 
